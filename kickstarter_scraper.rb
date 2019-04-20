5dd900b815aaa85f37bd2c309234cf731d541dab
Natasha Chernis <natashachernis@gmail.com>
2019-04-20 04:50:25 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,7 @@ require 'pry'
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text.strip
 # locatiion: project.css("ul.project-meta li a").text.strip
+# percent funded: project.css("ul.project-stats li.first.funded strong").text.sub("%", "").to_i
 
 def create_project_hash
 
