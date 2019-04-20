83ae87f3ab57fec5a7d19b5ae5d80452890256b5
Natasha Chernis <natashachernis@gmail.com>
2019-04-20 05:02:31 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,26 @@ require 'pry'
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.sub("%", "").to_i
 
+def title
+  project.css("h2.bbcard_name strong a").text
+end
+
+def image_link
+  project.css("div.project-thumbnail a img").attribute("src").value
+end
+
+def description
+  project.css("p.bbcard_blurb").text.strip
+end
+
+def location
+  project.css("ul.project-meta span.location-name").text
+end
+
+def percent_funded
+  project.css("ul.project-stats li.first.funded strong").text.sub("%", "").to_i
+end
+
 def create_project_hash
 
   html = File.read('fixtures/kickstarter.html')
@@ -15,7 +35,9 @@ def create_project_hash
 
   projects = kickstarter.css("li.project.grid_4")
   project_hash = {}
-  
+  projects.each do |project|
+    project_hash[]
+  end
 
 end
 
